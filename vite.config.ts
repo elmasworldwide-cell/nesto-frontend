21f19cb824524b37320b93474923ee48779be185
elmasworldwide-cell <elmasworldwide@gmail.com>
2026-05-06 11:19:41 +0300

    fix: build success vite v8

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          "react-vendor": ["react", "react-dom", "react-router-dom"],
-          "query-vendor": ["@tanstack/react-query"],
-          "axios-vendor": ["axios"],
-        },
-      },
-    },
     chunkSizeWarningLimit: 1000,
   },
   server: {
